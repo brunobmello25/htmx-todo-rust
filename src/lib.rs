db329a34cd92f14ec99b12c215e364fe56aaec60
Bruno Mello <bruno.barros.mello@gmail.com>
2024-01-20 23:34:02 -0300

    wip: trying to figure out how the hell askama works

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+use askama::Template;
+use axum_core::response::{IntoResponse, Response};
+use http::StatusCode;
+
+pub fn into_response<T: Template>(t: &T) -> Response {
+    match t.render() {
+        Ok(body) => {
+            let headers = [(
+                http::header::CONTENT_TYPE,
+                http::HeaderValue::from_static(T::MIME_TYPE),
+            )];
+
+            (headers, body).into_response()
+        }
+        Err(_) => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
+mod templates;
+
 use axum::{
     routing::get,
     Router,
 };
+use templates::hello::HelloTemplate;
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().route("/", get(|| async { 
+        HelloTemplate {
+            name: "bruno"
+        }
+    }));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
